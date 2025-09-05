e8a6eaee7c8792f24ddcf1740d0bc8af4140c273
Petko Todorov <p.todorov1015@gmail.com>
2025-09-05 10:44:18 +0300

    Initial commit

---
+++ 30 Days of Code/Day 9 - Recursion 3.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    result = factorial(n)
+    print(result)
