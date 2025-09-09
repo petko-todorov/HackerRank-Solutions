a55350227d9dc6c78daca7d268744f183eb98da7
Petko Todorov <p.todorov1015@gmail.com>
2025-09-09 10:07:49 +0300

    Initial commit

---
+++ 30 Days of Code/Day 13 - Abstract Classes.py
@@ -0,0 +1,28 @@
+from abc import ABCMeta, abstractmethod, ABC
+
+
+class Book(object, metaclass=ABCMeta):
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @abstractmethod
+    def display(self): pass
+
+
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        super().__init__(title, author)
+        self.price = price
+
+    def display(self):
+        print(f"""Title: {self.title}
+Author: {self.author}
+Price: {self.price}""")
+
+
+# title = input()
+# author = input()
+# price = int(input())
+# new_novel = MyBook(title, author, price)
+# new_novel.display()
