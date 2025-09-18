c6a6f0ce2d6071771c2772fb06ea2619fe3a77f2
Petko Todorov <p.todorov1015@gmail.com>
2025-09-18 13:56:28 +0300

    Add 2 components

--- Testdome/react/src/App.jsx
+++ Testdome/react/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import CharacterAttributes from './components/CharacterPoints';
 import TodoList from './components/TodoList';
+import FocusableInput from './components/FocusableInput';
+import Message from './components/ToggleMessage';
 
 const items = [
     { text: 'Buy grocery', done: true },
@@ -12,6 +14,7 @@ function App() {
     return (
         <>
             <CharacterAttributes totalPoints={15} />
+            <hr />
             <TodoList
                 items={items}
                 onListClick={(event) => console.log('List clicked!')}
@@ -19,6 +22,10 @@ function App() {
                     console.log(item, event);
                 }}
             />
+            <hr />
+            <FocusableInput shouldFocus={true} />
+            <hr />
+            <Message />
         </>
     );
 }
